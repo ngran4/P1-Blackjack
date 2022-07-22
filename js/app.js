abf50a1b46203ed9090f29067c8d3136ee408027
ngran4 <nicgranville@gmail.com>
2022-07-22 12:05:36 -0700

    add cardstarter

---
+++ js/app.js
@@ -0,0 +1,58 @@
+/*----- constants -----*/
+const deck = {};
+
+const shuffledContainer;
+
+const stayBtn;
+
+const hitBtn
+
+const betBtn; (? different buttons for different bets—i.e chip values?
+
+/*----- app's state (variables) -----*/
+let scores; 
+{
+    player: 0;
+    dealer: 0;
+}
+
+let winner;
+
+let shuffledDeck;
+
+let bank;
+
+let playerHand;
+
+let dealerHand;
+
+let handTotal; (dealer, player)
+
+let doubleBtn;
+
+/*----- cached element references -----*/
+// deckEl
+
+// scoresEl(player, dealer)
+
+
+/*----- event listeners -----*/
+// stayBtn;
+
+// hitBtn;
+
+// betBtn;
+
+// doubleBtn;
+
+
+/*----- functions -----*/
+
+// render()
+
+// init()
+
+// hit()
+
+// deal()
+
